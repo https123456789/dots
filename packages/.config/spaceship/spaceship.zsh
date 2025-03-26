59c14a87695b6327ac28ad626369180699c32688
Ben Landon <landb0x207@gmail.com>
2025-03-26 15:55:06 -0700

    feat: spaceship config

---
+++ packages/.config/spaceship/spaceship.zsh
@@ -0,0 +1,12 @@
+SPACESHIP_USER_SHOW=true
+SPACESHIP_GIT_BRANCH_ASYNC=true
+
+SPACESHIP_CHAR_SYMBOL="->"
+SPACESHIP_CHAR_SUFFIX=" "
+
+# **Show the full compiler version**
+# The docs:
+# > By default, this section will strip all of the suffixes from the version.
+# > For example, 1.42.0-nightly.2020-06-09 will be displayed as 1.42.0.
+# So we enable this option to show everything
+SPACESHIP_RUST_VERBOSE_VERSION=true
